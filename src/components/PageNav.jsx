ab296c7c1493fe09906ba5c5cc26d1347fa6af49
Khalid-MahmouD <khaloooood827@gmail.com>
2025-02-08 22:21:53 +0200

    reduced

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,30 +1,29 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
-import { ToastContainer } from "react-toastify";
 
 function PageNav() {
   return (
     <>
       <nav className={styles.nav}>
-      <Logo />
-      <ul>
-        <li>
-          <NavLink to="/pricing">Pricing</NavLink>
-        </li>
-        <li>
-          <NavLink to="/product">Product</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" className={styles.ctaLink}>
-            Login
-          </NavLink>
-        </li>
-        <li>
+        <Logo />
+        <ul>
+          <li>
+            <NavLink to="/pricing">Pricing</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className={styles.ctaLink}>
+              Login
+            </NavLink>
+          </li>
+          <li>
 
-        </li>
-      </ul>
-    </nav>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
